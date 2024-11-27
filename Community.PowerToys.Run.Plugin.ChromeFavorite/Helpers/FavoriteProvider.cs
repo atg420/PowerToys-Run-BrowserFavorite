0edddb2bd1426bfa5b540e1f7d52a855c948420d
DerPenz <99838921+Der-Penz@users.noreply.github.com>
2024-11-27 11:30:15 +0000

    changed path to brave browser

--- Community.PowerToys.Run.Plugin.ChromeFavorite/Helpers/FavoriteProvider.cs
+++ Community.PowerToys.Run.Plugin.ChromeFavorite/Helpers/FavoriteProvider.cs
@@ -11,7 +11,7 @@ namespace Community.PowerToys.Run.Plugin.ChromeFavorite.Helpers
 {
     public class FavoriteProvider : IFavoriteProvider
     {
-        private readonly string _path = Environment.ExpandEnvironmentVariables(@"%LOCALAPPDATA%\Google\Chrome\User Data\Default\Bookmarks");
+        private readonly string _path = Environment.ExpandEnvironmentVariables(@"%LOCALAPPDATA%\BraveSoftware\Brave-Software\User Data\Default\Bookmarks");
         private readonly FileSystemWatcher _watcher;
         private FavoriteItem _root;
 
